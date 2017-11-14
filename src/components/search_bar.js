ccaea2cf2aad2b1fa9f8c273a982b00a58281b3f
bobby <bwuphan@gmail.com>
2017-11-13 23:02:04 -0800

    Throttling search term input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,15 +7,40 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
 
+  onSearchChange(term) {
+    console.log(term);
+    this.setState({ term });
+    debugger;
+    this.props.onSearchTermChange(term);
+  }
+
   render() {
     return (
-      <div className="search-bar">
+      <div>
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onSearchChange(event.target.value)} />
       </div>
     );
   }
 }
 
 export default SearchBar;
+
+
+// Take an array and replace every 0 with null
+
+function test(expected, actual) {
+  if (expected === actual) {
+    console.log('Test passed!');
+  } else {
+    console.log(`Test failed, expected ${expected} to equal ${actual}`);
+  }
+}
+
+function nullZeros(array) {
+  //Your code here
+
+}
+
+test()
